b56bb6d1346b41522812f00eb3db2866fc1bd35d
Lalu Saud <lalusaud@gmail.com>
2018-05-18 03:43:14 -0400

    Bugfix on delete station

--- app/models/station.rb
+++ app/models/station.rb
@@ -3,5 +3,7 @@ class Station < ApplicationRecord
   mount_uploader :logo, LogoUploader
 
   has_and_belongs_to_many :categories
+  before_destroy { categories.clear }
+  
   has_many :streams, dependent: :destroy
 end
