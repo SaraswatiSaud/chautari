beee43d763c8b10bbe8b871359e2f47d1fd1a8ec
Lalu Saud <lalusaud@gmail.com>
2018-05-18 22:24:35 -0400

    UI fixes and pagination fixes in Mobile App

--- app/models/station.rb
+++ app/models/station.rb
@@ -4,6 +4,8 @@ class Station < ApplicationRecord
 
   has_and_belongs_to_many :categories
   before_destroy { categories.clear }
-  
+
   has_many :streams, dependent: :destroy
+
+  paginates_per 15
 end
