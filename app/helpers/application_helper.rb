9bb6bbf7f97e85630a1dc1f291001a177805ca3b
Lalu Saud <lalusaud@gmail.com>
2018-05-20 23:07:18 -0400

    Change title of the pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def title(page_title = '')
+    if page_title.present?
+      "#{page_title} | Listen Radio Online | Chautari"
+    else
+      'Listen to Radio Stations from the globe | Chautari'
+    end
+  end
 end
