33eaced7ca168f928972d31bd31a4377a8f3fce9
Lalu Saud <lalusaud@gmail.com>
2018-05-28 01:00:17 -0400

    Display Country Name

--- app/models/station.rb
+++ app/models/station.rb
@@ -6,18 +6,20 @@ class Station < ApplicationRecord
   before_destroy { categories.clear }
 
   has_many :streams, dependent: :destroy
-  # belongs_to :language
-  # belongs_to :country
+  belongs_to :language
+  belongs_to :country
 
   paginates_per 15
 
   extend FriendlyId
   friendly_id :name, use: :slugged
 
-  validates :name, :country, presence: true
+  validates :name, :country_id, :language_id, presence: true
   before_validation :strip_whitespace
 
   accepts_nested_attributes_for :streams, reject_if: :all_blank, allow_destroy: true
+
+  delegate :name, to: :country, prefix: true
 
   private
   def strip_whitespace
