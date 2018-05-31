9d6e3d98997fd600f0a414139ba59aa9da8240f4
Lalu Saud <lalusaud@gmail.com>
2018-05-31 11:20:28 -0400

    UI Fixes for social share

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  after_action :set_vary_header
 
   def authenticate_admin!
     authenticate_user!
     redirect_to root_path, alert: 'You are not authorized to this action!' unless current_user.is_admin?
+  end
+
+  private
+
+  def set_vary_header
+    response.headers['Vary'] = 'accept' if request.xhr?
   end
 end
