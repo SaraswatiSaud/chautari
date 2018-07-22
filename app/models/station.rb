8e9ef425aaeb974423c650d7e36b8875c83c7ed9
Lalu Saud <lalusaud@gmail.com>
2018-07-22 17:52:50 -0400

    Refactor station meta tags

--- app/models/station.rb
+++ app/models/station.rb
@@ -26,7 +26,12 @@ class Station < ApplicationRecord
   delegate :title, to: :language, prefix: true, allow_nil: true
 
   def similar_stations
-    Station.active.where(country_id: country_id).or(Station.where(language_id: language_id)).or(Station.where(categories: categories)).limit(5)
+    Station.active.where(country_id: country_id)
+      .or(Station.where(language_id: language_id))
+      .or(Station.where(categories: categories))
+      .where.not(id: self.id)
+      .order('RANDOM()')
+      .limit(5)
   end
 
   def playing_now
@@ -39,7 +44,39 @@ class Station < ApplicationRecord
     streams.active
   end
 
+  def full_address
+    @full_address ||= join_array([address, country_name])
+  end
+
+  def categories_names
+    @categories_names ||= join_array(categories.pluck(:title))
+  end
+
+  def meta_title
+    @meta_title ||= "Listen to #{name}, #{full_address} | Jharko"
+  end
+
+  def meta_desc
+    @meta_desc ||= (description? ? description.squish : default_meta_desc)
+  end
+
+  def meta_keywords
+    @meta_keywords ||= join_array([name, tagline, 'free radio', categories_names, 'stream music', full_address, language_title.to_s + ' radio'].flatten)
+  end
+
   private
+
+  def default_meta_desc
+    str = "#{name} is a Radio Station streaming online from #{full_address}."
+    str += "This station plays content of categories - #{categories_names}. " if categories_names.present?
+    str += "Listen to #{name} for free online."
+    str
+  end
+
+  def join_array(arr)
+    arr.compact.reject(&:empty?).join(', ')
+  end
+
   def strip_whitespace
     self.name.try(:strip!)
   end
