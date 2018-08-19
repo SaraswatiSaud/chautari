27468d3a164960bd4c3b19beb80b9b9aef983424
Lalu Saud <lalusaud@gmail.com>
2018-08-19 00:45:14 -0400

    Add structured data for search engines

---
+++ app/helpers/stations_helper.rb
@@ -0,0 +1,15 @@
+module StationsHelper
+  def google_structured_data station
+    data = {
+      "@context": "http://schema.org",
+      "@type": "RadioStation",
+      "name": station.name.to_s,
+      "url": station_url(station),
+      "image": ENV['ROOT_URL'] + station.logo_url,
+      "address": station.full_address,
+      "priceRange": "-",
+      "telephone": "-",
+    }
+    javascript_tag data.to_json, type: 'application/ld+json'
+  end
+end
