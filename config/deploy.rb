eded756e4e9bc2dae1ff24a16d62b98a92b3098a
Lalu Saud <lalusaud@gmail.com>
2020-01-28 23:51:44 -0500

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,7 +47,7 @@ namespace :puma do
 end
 
 namespace :deploy do
-  # desc "Make sure local git is in sync with remote."
+  desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
       unless `git rev-parse HEAD` == `git rev-parse origin/master`
@@ -58,7 +58,7 @@ namespace :deploy do
     end
   end
 
-  # desc 'Initial Deploy'
+  desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
       before 'deploy:restart', 'puma:start'
